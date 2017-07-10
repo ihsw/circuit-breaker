4ddbf800317c3e5a0de7a5b08f751fae84f2059b
Adrian Parker <ihsw.aparker@gmail.com>
2017-07-10 13:52:31 -0400

    Start circuit-breaker lib.

---
+++ app/src/lib/circuit-breaker.ts
@@ -0,0 +1,47 @@
+export interface ISettings {
+  decrementDuration: number;
+  upperThreshold: number;
+  cooloffPeriod: number;
+}
+
+export default class {
+  // internal state
+  counter: number;
+  isOpen: boolean;
+
+  // options
+  decrementDuration: number;
+  upperThreshold: number;
+  cooloffPeriod: number;
+
+  constructor(opts: ISettings) {
+    this.decrementDuration = opts.decrementDuration;
+    this.upperThreshold = opts.upperThreshold;
+    this.cooloffPeriod = opts.cooloffPeriod;
+
+    this.counter = 0;
+    this.isOpen = false;
+  }
+
+  increment() {
+    if (this.isOpen) {
+      return;
+    }
+
+    this.counter++;
+
+    if (this.counter > this.upperThreshold) {
+      this.isOpen = true;
+      setTimeout(() => this.reset(), this.cooloffPeriod);
+
+      return;
+    }
+
+    setTimeout(() => { this.counter--; }, this.decrementDuration);
+  }
+
+  reset() {
+    this.counter = 0;
+    this.isOpen = false;
+  }
+}
---
+++ app/src/test/circuit-breaker.ts
@@ -0,0 +1,57 @@
+import { test } from "ava";
+
+import { default as CircuitBreaker } from "../lib/circuit-breaker";
+
+const pause = (delay: number): Promise<void> => new Promise<void>((resolve) => setTimeout(resolve, delay));
+
+test("Circuit breaker default values should be sane", async (t) => {
+  const breaker = new CircuitBreaker({ cooloffPeriod: 0, decrementDuration: 0, upperThreshold: 0 });
+  t.is(breaker.isOpen, false);
+  t.is(breaker.counter, 0);
+});
+
+test("Circuit breaker default values should increment and decrement over a given period", async (t) => {
+  // setting up the circuit breaker
+  const upperThreshold = 5;
+  const decrementDuration = 1*1000;
+  const breaker = new CircuitBreaker({
+    cooloffPeriod: 5*1000,
+    decrementDuration: decrementDuration,
+    upperThreshold: upperThreshold
+  });
+
+  // incrementing the breaker up
+  for (let i = 0; i < upperThreshold; i++) {
+    breaker.increment();
+    t.is(breaker.isOpen, false);
+    t.is(breaker.counter, i+1);
+  }
+
+  // waiting for the breaker to drain out the counter
+  await pause(decrementDuration+10);
+
+  // expecting the counter to be back to normal
+  t.is(breaker.counter, 0);
+});
+
+test("Circuit breaker should break and cool off", async (t) => {
+  // setting up the circuit breaker
+  const upperThreshold = 5;
+  const decrementDuration = 1*1000;
+  const cooloffPeriod = 5*1000;
+  const breaker = new CircuitBreaker({
+    cooloffPeriod: cooloffPeriod,
+    decrementDuration: decrementDuration,
+    upperThreshold: upperThreshold
+  });
+
+  // incrementing the breaker up to trip it
+  for (let i = 0; i < upperThreshold+1; i++) {
+    breaker.increment();
+  }
+  t.is(breaker.isOpen, true);
+
+  // waiting for the breaker to cool off
+  await pause(cooloffPeriod+10);
+  t.is(breaker.isOpen, false);
+});
